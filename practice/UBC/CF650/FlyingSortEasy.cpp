aee81e3a2f19832a646ff1579518890a97eb1bb7
xyzTiger <980401@gmail.com>
2020-10-10 12:23:28 -0700

    Solved A, B, C, D in UBC practice (CF Round 650).

---
+++ practice/UBC/CF650/FlyingSortEasy.cpp
@@ -0,0 +1,22 @@
+//
+// Created by TigerZhao on 2020-09-27.
+//
+
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+
+    }
+  }
+  return 0;
+}
---
+++ practice/UBC/CF650/NecklaceAssembly.cpp
@@ -0,0 +1,13 @@
+//
+// Created by TigerZhao on 2020-09-27.
+//
+
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+  return 0;
+}
